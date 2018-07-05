5ba71874fd52c9b23f431819c3edeedb6c613083
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-05 11:31:11 +0900

    add monkey patch

--- lib/Koyaaan.rb
+++ lib/Koyaaan.rb
@@ -1,5 +1,20 @@
 require "Koyaaan/version"
 
 module Koyaaan
-  # Your code goes here...
+  def self.setup(&proc)
+    # create function for Monkey patch
+    extend self
+    (
+    class << self;
+      self
+    end).module_eval do
+      define_method 'convert_toot', &proc
+      # define_method 'b' do
+      #   p 'b'
+      # end
+    end
+
+    # Monkey patch
+    Api::V1::StatusesController.prepend(ApiV1StatusesControllerPatch)
+  end
 end
