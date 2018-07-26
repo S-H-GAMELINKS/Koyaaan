942ecd2b80ab98f82374da2cfb92a0732ca40367
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-07-26 11:11:47 +0900

    fix

--- lib/Koyaaan/convert_toot.rb
+++ lib/Koyaaan/convert_toot.rb
@@ -2,7 +2,7 @@ module Koyaaan
     class Random < Koyaaan::Convert
   
       def convert(input)
-        input.replace(@replaces.sample)
+        input.replace(@replaces.replace)
       end
     end
   end
